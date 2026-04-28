d12022a8b1b0f8e134e03eff95c0f7c9ff1dab04
Gustek <gustek@riseup.net>
2026-04-28 16:50:47 +0200

    Implement typechecking for applications

--- src/Sefer/Typecheck.hs
+++ src/Sefer/Typecheck.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TupleSections #-}
 module Sefer.Typecheck ( Typechecker(..), TCError(..), initTC, runTC ) where
 
 import Control.Applicative
@@ -22,6 +22,8 @@ import qualified Data.List.Extra as Data.List
 import Sefer.Type (TypeVar)
 import qualified Control.Exception as Type
 import Sefer.Expr (Expr(fargs))
+import Data.Traversable (for)
+import Data.Functor
 
 data Typechecker = Typechecker
                    { vg :: Int
@@ -40,7 +42,7 @@ data TCError = HeteroPrim Type.Type Type.Type
              | WrongADT Int Int
              | UnifyFail Type.Type Type.Type
              | InfiniteType Type.Type
---             | MissingAnnotations Expr.LocExprT Expr.LocExprT Int
+             | MissingAnnotations Expr.LocExprT Expr.LocExprT Int
              deriving (Show, Eq)
 
 type Result = Either (Located TCError)
@@ -56,8 +58,10 @@ defaultBindings :: Map Text Type.Type
 defaultBindings = Data.HashMap.fromList [ (pack "$addInt", Type.Fun [Type.Integer, Type.Integer] Type.Integer)
                                         ]
 
-runTC :: StateT Typechecker Result ()
-runTC = pure ()
+
+
+runTC :: Expr.LocExprT -> StateT Typechecker Result Expr.LocExprT
+runTC = flip (.) tcExpr $ (=<<) applySubsts
 
 -- utilities
 
@@ -67,7 +71,7 @@ newVar = do
   put tc { vg = vg + 1 }
   pure (vg, lvl)
 
-occurs :: Location -> TypeVar -> Type.Type -> StateT Typechecker Result Type.Type 
+occurs :: Location -> TypeVar -> Type.Type -> StateT Typechecker Result Type.Type
 occurs loc v t@(Type.Variable v')
   |  v == v' = throwError (loc, InfiniteType t)
 occurs loc (vi, vl) (Type.Variable (i, l)) = do
@@ -76,12 +80,55 @@ occurs loc (vi, vl) (Type.Variable (i, l)) = do
     Just t -> occurs loc (vi, vl) t
     Nothing -> let l' = if member vi substs then l else min l vl
                in pure $ Type.Variable (i, l')
-occurs loc v (Type.Tuple ts) = Type.Tuple <$> mapM (occurs loc v) ts 
+occurs loc v (Type.Tuple ts) = Type.Tuple <$> mapM (occurs loc v) ts
 occurs loc v (Type.Fun xs y) = Type.Fun <$> mapM (occurs loc v) xs <*> occurs loc v y
 occurs loc v (Type.Data i ts) = Type.Data i <$> mapM (occurs loc v) ts
 occurs _ _ t = pure t
 
+-- applySubstsT :: Map Int Type.Type -> Type.Type -> StateT Typechecker Result Type.Type
+-- applySubstsT m (Type.Tuple ts) = Type.Tuple <$> mapM (applySubstsT m) ts
+-- applySubstsT m (Type.Fun xs y) = Type.Fun <$> mapM (applySubstsT m) xs <*> applySubstsT m y
+-- applySubstsT m (Type.Data i ts) = Type.Data i <$> mapM (applySubstsT m) ts
+-- applySubstsT m t@(Type.Variable (i, _)) = case Data.HashMap.lookup i m of
+--                                      Nothing -> pure t
+--                                      Just t' -> applySubstsT m t'
+-- applySubstsT _ t = pure t
+
+-- applySubsts' :: Type.LocAnnT -> Expr.LocExprT -> StateT Typechecker Result Expr.LocExprT
+-- applySubsts' st (Expr.Literal _ lit) = pure $ Expr.Literal st lit
+-- applySubsts' st (Expr.Var _ x) = pure $ Expr.Var st x
+-- applySubsts' st (Expr.Let _ (x, (xt, xl)) y z) = do
+--   Typechecker { substs = m } <- get
+--   xt' <- for xt $ applySubstsT m
+--   y' <- applySubsts y
+--   z' <- applySubsts z
+--   pure $ Expr.Let st (x, (xt', xl)) y' z'
+-- applySubsts' st (Expr.Cond _ ei et ee) = Expr.Cond st <$> applySubsts ei <*> applySubsts et <*> applySubsts ee
+-- applySubsts' st (Expr.App _ f args) = Expr.App st <$> applySubsts f <*> mapM applySubsts args
+-- applySubsts' st (Expr.Abs _ fargs e) = do
+--   Typechecker { substs = m } <- get
+--   fargs' <- forM fargs $ \(x, (t, l)) -> for t (applySubstsT m) <&> (x,) . (,l)
+--   e' <- applySubsts e
+--   pure $ Expr.Abs st fargs' e'
+-- applySubsts' st (Expr.Tuple _ xs) = Expr.Tuple st <$> mapM applySubsts xs
+-- applySubsts' st (Expr.Const _ i xs) = Expr.Const st i <$> mapM applySubsts xs
+-- applySubsts' _ (Expr.Match _ _ _) = error "todo"
+
+-- applySubsts :: Expr.LocExprT -> StateT Typechecker Result Expr.LocExprT
+-- applySubsts e = do
+--   Typechecker { substs = m } <- get
+--   let (t, loc) = Expr.ann e
+--   t' <- for t $ applySubstsT m
+--   e' <- applySubsts' (t', loc) e
+--   case undecided e' of
+--     Nothing -> pure e'
+--     Just (i, e'') -> throwError (snd . Expr.ann $ e', MissingAnnotations  e' e'' i)
+
+-- undecided :: Expr.LocExprT -> Maybe (Int, Expr.LocExprT)
+-- undecided _ = Nothing
+
 -- typechecking
+
 tvUnify :: Location -> TypeVar -> Type.Type -> StateT Typechecker Result ()
 tvUnify loc (vi, vl) t = do
   tc@Typechecker { substs } <- get
@@ -154,11 +201,11 @@ tcExpr e@(Expr.Var (t, loc) x) = do
     Just t' -> case t of
                  Nothing -> tcExpr $ Expr.Var (Just t', loc) x
                  Just t -> unify loc t t' >> pure e
-tcExpr e@(Expr.Abs (t, loc) fargs expr) = do
+tcExpr (Expr.Abs (t, loc) fargs expr) = do
   -- fresh type variables for arguments
   tfargs <- mapM (const $ Type.Variable <$> newVar) fargs
   -- unify type variables against the declared type of arguments
-  let fargs' = Prelude.map (\((x, (t, loc)), t') -> (x, (Just $ t' `fromMaybe` t, loc))) $ Prelude.zip fargs tfargs
+  let fargs' = Prelude.zipWith (\(x, (t, loc)) t' -> (x, (Just $ t' `fromMaybe` t, loc))) fargs tfargs
   mapM_ (\(t, (_, (t', loc))) -> unify loc t $ fromJust t') $ Prelude.zip tfargs fargs'
   tc@Typechecker { bindings } <- get
   let nb = Prelude.foldl (flip (\((x, _), t') -> insert x t')) bindings $ Prelude.zip fargs tfargs
@@ -171,4 +218,13 @@ tcExpr e@(Expr.Abs (t, loc) fargs expr) = do
   let t' = Type.Fun tfargs $ fromJust . fst . Expr.ann $ expr'
   case t of
     Nothing -> tcExpr $ Expr.Abs (Just t', loc) fargs' expr'
-    Just t -> unify loc t t' >> pure e
+    Just t -> unify loc t t' >> pure (Expr.Abs (Just t, loc) fargs' expr')
+tcExpr (Expr.App (t, loc) f cargs) = do
+  f' <- tcExpr f
+  cargs' <- mapM tcExpr cargs
+  t' <- Type.Variable <$> newVar
+  unify loc (fromJust . fst . Expr.ann $ f') $ flip Type.Fun t' $ Prelude.map (fromJust . fst . Expr.ann) cargs'
+  case t of
+    Nothing -> tcExpr $ Expr.App (Just t', loc) f' cargs'
+    Just t -> unify loc t t' >> pure (Expr.App (Just t, loc) f' cargs')
+tcExpr _ = error "todo"
